6608736e280f98c32c41ca6fa173ff4966acbe28
Chrishart <chrishart.estrada@gmail.com>
2024-04-30 11:58:07 +0800

    i can tweet the quotes now!

--- the-projects/1-quote-generator/script.js
+++ the-projects/1-quote-generator/script.js
@@ -1,10 +1,28 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 let apiQuotes = [];
 
 // Show New Quote
 function newQuote(){
     // Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+    // Check if Author field is blank and replace with 'Unknown'
+    if(!quote.author){
+        authorText.textContent = 'Unknown';
+    }else{
+        authorText.textContent = quote.author;
+    }
+
+    // Check Quote length to determine styling
+    if(quote.text.length > 100){
+        quoteText.classList.add('long-quote');
+    }quoteText.classList.remove('long-quote');
+
+    quoteText.textContent = quote.text;
 }
 
 // Get Quotes From API
@@ -20,6 +38,16 @@ async function getQuotes(){
         // Catch Error Here
     }
 }
+
+// Tweet Quote
+function tweetQuote(){
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
 getQuotes();
