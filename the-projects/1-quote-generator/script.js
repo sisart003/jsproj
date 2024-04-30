292c030ce446a49f26babad6ad72005e5b4a9f5b
Chrishart <chrishart.estrada@gmail.com>
2024-04-30 12:22:08 +0800

    adding loader

--- the-projects/1-quote-generator/script.js
+++ the-projects/1-quote-generator/script.js
@@ -3,11 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// Show Loading
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete(){
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 // Show New Quote
 function newQuote(){
+    loading();
     // Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     // Check if Author field is blank and replace with 'Unknown'
@@ -20,13 +34,16 @@ function newQuote(){
     // Check Quote length to determine styling
     if(quote.text.length > 100){
         quoteText.classList.add('long-quote');
-    }quoteText.classList.remove('long-quote');
-
+    }else{
+        quoteText.classList.remove('long-quote');
+    }
     quoteText.textContent = quote.text;
+    complete();
 }
 
 // Get Quotes From API
 async function getQuotes(){
+    loading();
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     
     try{
@@ -34,8 +51,8 @@ async function getQuotes(){
         apiQuotes = await response.json();
         newQuote();
     }catch(error){
-
         // Catch Error Here
+        console.log(error)
     }
 }
 
