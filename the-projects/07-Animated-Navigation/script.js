2ad1afdad6a8f880d5285e0f5de8eb00b72c562f
Chrishart <chrishart.estrada@gmail.com>
2024-05-12 20:00:12 +0800

    animation slide delay added

--- the-projects/07-Animated-Navigation/script.js
+++ the-projects/07-Animated-Navigation/script.js
@@ -16,10 +16,42 @@ function toggleNav(){
         // Animate In - Overlay
         overlay.classList.remove('overlay-slide-left')
         overlay.classList.add('overlay-slide-right');
+
+        // Animate In - Nav Items
+        nav1.classList.remove('slid-out-1');
+        nav1.classList.add('slide-in-1');
+
+        nav2.classList.remove('slid-out-2');
+        nav2.classList.add('slide-in-2');
+
+        nav3.classList.remove('slid-out-3');
+        nav3.classList.add('slide-in-3');
+
+        nav4.classList.remove('slid-out-4');
+        nav4.classList.add('slide-in-4');
+
+        nav5.classList.remove('slid-out-5');
+        nav5.classList.add('slide-in-5');
     }else{
         // Animate Out - Overlay
         overlay.classList.remove('overlay-slide-right');
         overlay.classList.add('overlay-slide-left');
+
+        // Animate In - Nav Items
+        nav1.classList.remove('slid-in-1');
+        nav1.classList.add('slide-out-1');
+
+        nav2.classList.remove('slid-in-2');
+        nav2.classList.add('slide-out-2');
+
+        nav3.classList.remove('slid-in-3');
+        nav3.classList.add('slide-out-3');
+
+        nav4.classList.remove('slid-in-4');
+        nav4.classList.add('slide-out-4');
+
+        nav5.classList.remove('slid-in-5');
+        nav5.classList.add('slide-out-5');
     }
 }
 
