4f7a3df72930e81417e127b4faeafc055e49501f
Chrishart <chrishart.estrada@gmail.com>
2024-05-21 07:22:27 +0800

    time duration added

--- the-projects/09-Video-Player/script.js
+++ the-projects/09-Video-Player/script.js
@@ -32,9 +32,19 @@ video.addEventListener('ended', showPlayIcon);
 
 // Progress Bar ---------------------------------- //
 
+// Calculate display time format
+function displayTime(time){
+    const minutes = Math.floor(time / 60)
+    let seconds = Math.floor(time % 60)
+    seconds = seconds > 9 ? seconds : `0${seconds}`
+    return `${minutes}:${seconds}`
+}
+
 // Update Progress bar as video plays
 function updateProgress(){
     progressBar.style.width = `${(video.currentTime / video.duration) * 100 }%`
+    currentTime.textContent = `${displayTime(video.currentTime)} /`
+    duration.textContent = `${displayTime(video.duration)}`
 }
 
 // Volume Controls --------------------------- //
