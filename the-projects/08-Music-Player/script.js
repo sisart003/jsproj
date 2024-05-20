16cf3edf7e5d5c9074192690fcb4fcf3041f6f60
Chrishart <chrishart.estrada@gmail.com>
2024-05-20 15:31:05 +0800

    prev and next button work

--- the-projects/08-Music-Player/script.js
+++ the-projects/08-Music-Player/script.js
@@ -1,7 +1,35 @@
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
+
 const music = document.querySelector('audio')
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
+
+// Music
+const songs = [
+    {
+        name: 'jacinto-1',
+        displayName: 'Electric Chill Machine',
+        artist: 'Cherry'
+    },
+    {
+        name: 'jacinto-2',
+        displayName: 'Seven Nation Army (Remix)',
+        artist: 'Mango'
+    },
+    {
+        name: 'jacinto-3',
+        displayName: 'Brahms (o2 Jam Remix)',
+        artist: 'Pineapple'
+    },
+    {
+        name: 'metric-1',
+        displayName: 'Front Row (Remix)',
+        artist: 'Salad'
+    }
+]
 
 // Check if playing
 let isPlaying = false;
@@ -24,3 +52,41 @@ function pauseSong(){
 
 // Play or Pause Event Listener
 playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()))
+
+// Update DOM
+function loadSong(song){
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    music.src = `music/${song.name}.mp3`;
+    image.src = `img/${song.name}.jpg`;
+}
+
+// Current Song
+let songIndex = 0;
+
+// Prev Song
+function prevSong(){
+    songIndex--;
+    if(songIndex < 0){
+        songIndex = songs.length -1;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+// Next Song
+function nextSong(){
+    songIndex++;
+    if(songIndex > songs.length -1){
+        songIndex = 0;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+// On Load - Select First Song
+loadSong(songs[songIndex]);
+
+// EventListeners
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
