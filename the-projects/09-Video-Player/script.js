89e6a5e2fc04dc84d7ec927a48b100375657bfee
Chrishart <chrishart.estrada@gmail.com>
2024-05-21 07:36:44 +0800

    progress click event

--- the-projects/09-Video-Player/script.js
+++ the-projects/09-Video-Player/script.js
@@ -47,6 +47,13 @@ function updateProgress(){
     duration.textContent = `${displayTime(video.duration)}`
 }
 
+// Click to seek within the video
+function setProgress(e){
+    const newTime = e.offsetX / progressRange.offsetWidth;
+    progressBar.style.width = `${newTime * 100}%`;
+    video.currentTime = newTime * video.duration;
+}
+
 // Volume Controls --------------------------- //
 
 
@@ -62,4 +69,5 @@ function updateProgress(){
 playBtn.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
 video.addEventListener('timeupdate', updateProgress);
-video.addEventListener('canplay', updateProgress)
+video.addEventListener('canplay', updateProgress);
+progressRange.addEventListener('click', setProgress);
