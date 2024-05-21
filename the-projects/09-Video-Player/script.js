aa880c1ce069f9fcb674172dcffba8afa8fa6fca
Chrishart <chrishart.estrada@gmail.com>
2024-05-21 09:04:56 +0800

    playbackrate of a video

--- the-projects/09-Video-Player/script.js
+++ the-projects/09-Video-Player/script.js
@@ -7,6 +7,7 @@ const volumeRange = document.querySelector('.volume-range')
 const volumeBar = document.querySelector('.volume-bar')
 const currentTime = document.querySelector('.time-elapsed')
 const duration = document.querySelector('.time-duration')
+const speed = document.querySelector('.player-speed')
 const fullScreenBtn = document.querySelector('.fullscreen')
 
 // Play & Pause ----------------------------------- //
@@ -105,7 +106,9 @@ function toggleMute(){
 
 // Change Playback Speed -------------------- //
 
-
+function changeSpeed(){
+    video.playbackRate = speed.value
+}
 
 // Fullscreen ------------------------------- //
 
@@ -118,3 +121,4 @@ video.addEventListener('canplay', updateProgress);
 progressRange.addEventListener('click', setProgress);
 volumeRange.addEventListener('click', changeVolume);
 volumeIcon.addEventListener('click', toggleMute);
+speed.addEventListener('change', changeSpeed);
